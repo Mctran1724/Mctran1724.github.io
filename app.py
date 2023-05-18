e639c5d171dd1ae0ef1b8603b2a691de8bcd88e7
Mctran1724 <Mctran1724@gmail.com>
2023-05-18 19:14:18 +0000

    html template

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") #refers to homepage
 def hello_world():
-  return "initial"
+  return render_template('home.html')
 
 
 #for testing
