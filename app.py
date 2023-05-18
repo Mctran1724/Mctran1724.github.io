ecf999308862a01f9cdcfe55180f1502e76fc281
Mctran1724 <Mctran1724@gmail.com>
2023-05-18 20:57:58 +0000

    reformatting main page

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/") #refers to homepage
+#homepage
+@app.route("/") 
 def hello_world():
   return render_template('home.html')
 
+#starforce calculator
+
+#yelp analysis
+
+#index fund analysis
 
 #for testing
 if __name__=='__main__':
